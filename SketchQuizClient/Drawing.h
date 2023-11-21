012366084ed86fb4dad5add704d860648a9a291c
Sung JI Yoon <66246253+SungJiYoon@users.noreply.github.com>
2023-11-21 15:42:46 +0900

    init

--- SketchQuizClient/Drawing.h
+++ SketchQuizClient/Drawing.h
@@ -1,65 +1,14 @@
 #pragma once
 
-#define SIZE_TOT 256                    // 전송 패킷(헤더 + 데이터) 전체 크기
-#define SIZE_DAT (SIZE_TOT-sizeof(int)) // 헤더를 제외한 데이터 부분만의 크기
+// 대화 상자를 만들고 표시하는 함수
+void CreateAndShowDialog(HWND hWnd);
 
-// 공통 메시지 형식
-// sizeof(COMM_MSG) == 256
-typedef struct _COMM_MSG
-{
-	int  type;
-	char dummy[SIZE_DAT];
-} COMM_MSG;
 
-// 채팅 메시지 형식
-// sizeof(CHAT_MSG) == 256
-typedef struct _CHAT_MSG
-{
-	int  type;
-	char msg[SIZE_DAT];
-} CHAT_MSG;
+// ======================= 지윤 =======================
 
-// 선 그리기 메시지 형식
-// sizeof(DRAWLINE_MSG) == 256
-typedef struct _DRAWLINE_MSG
-{
-	int  type;
-	int  color;
-	int  x0, y0;
-	int  x1, y1;
-	char dummy[SIZE_TOT - 6 * sizeof(int)];
-} DRAWLINE_MSG;
 
-// 그림 지우기 메시지 형식
-// sizeof(ERASEPIC_MSG) == 256
-typedef struct _ERASEPIC_MSG
-{
-	int  type;
-	char dummy[SIZE_DAT];
-} ERASEPIC_MSG;
 
-/* 통신 관련 전역 변수 */
-static volatile bool g_isIPv6;        // IPv4 or IPv6 주소
-static char          g_ipaddr[64];    // 서버 IP 주소(문자열)
-static int           g_port;          // 서버 포트 번호
-static volatile bool g_isUDP;         // TCP or UDP 프로토콜
-static HANDLE        g_hClientThread; // 스레드 핸들
-static volatile bool g_bCommStarted;  // 통신 시작 여부
-static SOCKET        g_sock;          // 클라이언트 소켓
-static HANDLE        g_hReadEvent;    // 이벤트 핸들(1)
-static HANDLE        g_hWriteEvent;   // 이벤트 핸들(2)
 
-/* 메시지 관련 전역 변수 */
-static CHAT_MSG      g_chatmsg;       // 채팅 메시지
-static DRAWLINE_MSG  g_drawlinemsg;   // 선 그리기 메시지
-static int           g_drawcolor;     // 선 그리기 색상
-static ERASEPIC_MSG  g_erasepicmsg;   // 그림 지우기 메시지
 
-// 대화상자(그림판) 프로시저
-INT_PTR CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
-// 자식 윈도우 프로시저
-LRESULT CALLBACK ChildWndProc(HWND, UINT, WPARAM, LPARAM);
-// 소켓 통신 스레드 함수
-DWORD WINAPI ClientMain(LPVOID arg);
-DWORD WINAPI ReadThread(LPVOID arg);
-DWORD WINAPI WriteThread(LPVOID arg);
+
+// ======================= 정호 =======================
--- SketchQuizClient/stdafx.h
+++ SketchQuizClient/stdafx.h
@@ -4,19 +4,41 @@
 #include "Winsock.h"
 #include "PrjClient.h"
 #include "Drawing.h"
-#include "DrawingAdd.h"
 
 /*리소스*/
 #include "resource.h" // 그림판 다이어로그창
 
 /*식별자*/
-#define SERVERIP4  _T("192.168.45.186")
+#define SERVERIP4  _T("127.0.0.1")
 #define SERVERIP6  _T("::1")
 #define SERVERPORT  9000
 
-#define TYPE_CHAT     1000              // 메시지 타입: 채팅
-#define TYPE_DRAWLINE 1001              // 메시지 타입: 선 그리기
-#define TYPE_ERASEPIC 1002              // 메시지 타입: 그림 지우기
+/* 윈도우 관련 전역 변수 */
+static HINSTANCE     g_hInstance;     // 프로그램 인스턴스 핸들
+static HWND          g_hBtnSendFile;  // [파일 전송] 버튼
+static HWND          g_hBtnSendMsg;   // [메시지 전송] 버튼
+static HWND          g_hEditStatus;   // 각종 메시지 출력 영역
+static HWND          g_hBtnErasePic;  // [그림 지우기] 버튼
+static HWND          g_hDrawWnd;      // 그림을 그릴 윈도우
+static HWND			 g_hMainWindow;   // 처음 윈도우창 핸들
+static HWND			 g_hDialog;       // 그림판 다이어로그 핸들
 
-#define WM_DRAWLINE (WM_USER+1)         // 사용자 정의 윈도우 메시지(1)
-#define WM_ERASEPIC (WM_USER+2)         // 사용자 정의 윈도우 메시지(2)
+// 다이얼로그 상태 플래그
+static bool          g_bDialogVisible = false;
+
+/* 통신 관련 전역 변수 */
+static volatile bool g_isIPv6;        // IPv4 or IPv6 주소
+static char          g_ipaddr[64];    // 서버 IP 주소(문자열)
+static int           g_port;          // 서버 포트 번호
+static volatile bool g_isUDP;         // TCP or UDP 프로토콜
+static HANDLE        g_hClientThread; // 스레드 핸들
+static volatile bool g_bCommStarted;  // 통신 시작 여부
+static SOCKET        g_sock;          // 클라이언트 소켓
+static HANDLE        g_hReadEvent;    // 이벤트 핸들(1)
+static HANDLE        g_hWriteEvent;   // 이벤트 핸들(2)
+
+/* 메시지 관련 전역 변수 */
+static CHAT_MSG      g_chatmsg;       // 채팅 메시지
+static DRAWLINE_MSG  g_drawlinemsg;   // 선 그리기 메시지
+static int           g_drawcolor;     // 선 그리기 색상
+static ERASEPIC_MSG  g_erasepicmsg;   // 그림 지우기 메시지
