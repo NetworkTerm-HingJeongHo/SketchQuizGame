be26b915a8fc1aad7498caf93e7f4568148686a9
김연경 <kimi070464@gmail.com>
2023-11-27 22:09:40 +0900

    .

--- SketchQuizClient/Game.h
+++ SketchQuizClient/Game.h
@@ -5,7 +5,7 @@
 
 
 
-void gameStart();
-void Display(HWND g_status, const char* fmt, ...);
-DWORD WINAPI TimerThread(LPVOID arg);
-void newRound();
+void gameStart();//게임 시작하는 함수
+void Display(HWND g_status, const char* fmt, ...);//EditText부분에 Text 표시
+DWORD WINAPI TimerThread(LPVOID arg); //타이머 스레드
+void newRound();  // 새 라운드 진행 시 실행
