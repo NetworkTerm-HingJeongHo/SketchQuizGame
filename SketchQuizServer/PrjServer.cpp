dac6b7c82050342bbc871f15921fda17be4c7a2b
Sung JI Yoon <66246253+SungJiYoon@users.noreply.github.com>
2023-12-02 15:53:11 +0900

    파일분리

--- SketchQuizServer/PrjServer.cpp
+++ SketchQuizServer/PrjServer.cpp
@@ -1,59 +1,20 @@
-#include "..\Common.h"
-#include "resource.h"
+#include "stdafx.h"
 
-#define SERVERPORT 9000
-#define BUFSIZE    256
-// ==== 정호 ====
-#define WM_SOCKET (WM_USER+1)
-
-// 소켓 정보 저장을 위한 구조체와 변수
-typedef struct _SOCKETINFO
-{
-	SOCKET sock;
-	char   buf[BUFSIZE + 1];
-	int    recvbytes;
-
-	// ===== 연경 ======
-	_TCHAR id_nickname[BUFSIZE]; // 사용자 닉네임
-	int    score=0;       // 게임 플레이 점수
-} SOCKETINFO;
-
-// ======= 연경 ======= 
-typedef struct _MESSAGEQUEUE {
-	char queue[BUFSIZE][100] = { NULL };         // 메시지 원형 큐: 이전 대화내용 표시. 꽉 차면 가장 오래된 메시지부터 지워진다.
-	int head = 0;                 // 원형 큐 인덱스
-	int tail = 0;
-} MESSAGEQUEUE;
-
-//char* g_msgQueue[BUFSIZE];    // 메시지 원형 큐: 이전 대화내용 표시. 꽉 차면 가장 오래된 메시지부터 지워진다.
-//int head = 0, tail = 0;           // 원형 큐 인덱스
-MESSAGEQUEUE g_msgQueue;
-// ====================
-
-// ==== 정호 ====
+// // =========== 정호 =============
 // 클라이언트 관리 배열
 int nTotalSockets = 0;
 int nTotalUDPSockets = 0;
-SOCKETINFO *SocketInfoArray[FD_SETSIZE];
+SOCKETINFO* SocketInfoArray[FD_SETSIZE];
 SOCKADDR_IN UDPSocketInfoArray[FD_SETSIZE];
 
-// 다이얼로그 프로시저
-INT_PTR CALLBACK DialogProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
-// 소켓 정보 관리 함수
-bool AddSocketInfoTCP(SOCKET sock);
-bool AddSocketInfoUDP(SOCKADDR_IN addr);
-void RemoveSocketInfo(int nIndex);
-void addMessage(char* message); 
-SOCKETINFO* GetSocketInfo(SOCKET sock);
-
-// =========== 정호 =============
 SOCKET listen_sock4;
 SOCKADDR_IN serveraddr;
 SOCKET socket_UDP;
 
-// 윈도우 메시지 처리 함수
-LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-void ProcessSocketMessage(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+// ============= 연경 =============== 
+//char* g_msgQueue[BUFSIZE];    // 메시지 원형 큐: 이전 대화내용 표시. 꽉 차면 가장 오래된 메시지부터 지워진다.
+//int head = 0, tail = 0;           // 원형 큐 인덱스
+MESSAGEQUEUE g_msgQueue;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
